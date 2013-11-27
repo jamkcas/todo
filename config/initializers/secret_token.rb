4a0fea1ad2649633ea498e0052c4b259f5aecb4e
james <jamkcas@yahoo.com>
2013-11-27 08:52:23 -0800

    Start of todo rails app. Added gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '268656c40f2891542c3608a587fd3f3db8a904e46da60bb1a111a490341eccdcf87b3b2a42e0ffc369d035844d4f813b9f99500f835333314fcebee5f28516ce'
